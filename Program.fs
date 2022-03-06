dd7ed8ee7a5d3b0c95fda0780af4dbb077dac8b3
Кирилл Андрев <hindmost.one@gmail.com>
2022-03-06 20:52:43 +0400

    Add GOTO table and stuff

--- Program.fs
+++ Program.fs
@@ -1,11 +1,9 @@
-﻿open System
-
-
-
-#nowarn "62"
+﻿#nowarn "62"
 #nowarn "40"
 
 module Option =
+  exception EmptyOption
+
   let elim
     (none : 'c)
     (some : 'a -> 'c)
@@ -14,6 +12,11 @@ module Option =
     = opt
       |> Option.map some
       |> Option.defaultValue none
+
+  let unsafeUnwrap
+    (opt : 'a option)
+         : 'a
+    = Option.defaultWith (fun () -> raise EmptyOption) opt
 
 /// Lexeme is a class of token.
 ///
@@ -28,7 +31,7 @@ module rec Lexeme =
 
   let show : Lexeme.t -> string =
     function
-    | Concrete t -> "'" + t + "'"
+    | Concrete t -> t
     | Category t -> "<" + t + ">"
 
 /// Terminals to be used in grammar.
@@ -80,12 +83,13 @@ module rec Point =
 
 module rec Rule =
   type t =
-    { points : Point.t list
+    { entity : NonTerm.t
+      points : Point.t list
       mark   : string
     }
 
   let show (rule : Rule.t) : string =
-    String.concat " " (List.map Point.show rule.points) + " {" + rule.mark + "}"
+    rule.mark + ".\t" + NonTerm.show rule.entity + " = " + String.concat " " (List.map Point.show rule.points)
 
 /// Grammar is an input for LR parser to generate all tables.
 ///
@@ -98,17 +102,31 @@ module rec Grammar =
 
   let show : Grammar.t -> string =
     fun grammar ->
-      let ruleToString (name, rules) =
-        String.concat "\n  | " <|
-          (  (NonTerm.show name + " =")
-          :: (rules |> List.map Rule.show)
-          )
-      
       grammar
-        |> Map.toList
-        |> Seq.map ruleToString
+        |> Map.values
+        |> Seq.map (List.map Rule.show)
+        |> List.concat
         |> String.concat "\n"
 
+  let addInternal
+    (entity  : NonTerm.t)
+    (mark    : string)
+    (points  : Point.t list)
+             : Grammar.t -> Grammar.t
+    =
+      Map.change entity
+        <| fun cases ->
+          Some
+            <| { entity = entity
+                 points = points
+                 mark   = mark
+               } :: Option.defaultValue [] cases
+
+  let addS : string -> Point.t list -> Grammar.t -> Grammar.t
+    = addInternal NonTerm.S 
+
+  let add (entity : string) : string -> Point.t list -> Grammar.t -> Grammar.t
+    = addInternal <| NonTerm.NonTerm entity
 /// Custom extensions for Sets.
 ///
 module Set =
@@ -144,12 +162,15 @@ module Set =
       then start
       else fixpoint step next
 
+  let bind (f : 'a -> 'b Set) (set : 'a Set) : 'b Set =
+    Set.map f set |> Set.unionMany
+
   let show1 (show : 'a -> string) (set : 'a Set) : string =
     set
       |> Set.map show
       |> Set.toList
       |> String.concat " "
-      |> (fun pts -> "{" + pts + "}")
+      //|> (fun pts -> "{" + pts + "}")
   
 /// Custom extensions for Maps.
 ///
@@ -231,7 +252,7 @@ module rec First =
   let show
     : First.t -> string
     = Map.toList
-    >> List.map (fun (s, terms) -> sprintf "%s = {%s}" (NonTerm.show s) (terms |> Set.toList |> List.map Term.show |> String.concat " "))
+    >> List.map (fun (s, terms) -> sprintf "%s = %s" (NonTerm.show s) (Set.show1 Term.show terms))
     >> String.concat "\n"
 
 /// Table of terminals that can be seen right after a non-terminal.
@@ -336,38 +357,256 @@ module rec Pos =
   let showOption (pos : Pos.t option) : string =
     pos |> Option.map show |> Option.defaultValue "-"
 
+/// Position of parser in relation to one rule.
+///
 module rec Item =
+
+  /// Carries name, points and mark from the rule.
+  /// Also contains lookahead.
+  ///
   type t =
-    { pos       : Pos.t
+    { entity    : NonTerm.t
+      pos       : Pos.t
       lookahead : Term.t Set
       mark      : string
     }
 
+  /// Create item from rule in the starting postion.
+  ///
+  let start (lookahead : Term.t Set) (rule : Rule.t) : Item.t =
+    { Item.entity    = rule.entity
+      Item.pos       = Pos.start rule.points
+      Item.lookahead = lookahead
+      Item.mark      = rule.mark
+    }
+
+  /// Move to the next point of the originating rule.
+  ///
+  let next (item : Item.t) : Item.t option =
+    Pos.next item.pos
+      |> Option.map (fun newPos -> { item with pos = newPos })
+
+  /// Get current point, if it has one.
+  ///
+  let locus (item : Item.t) : Point.t option =
+    item.pos.locus
+
   let show (item : Item.t) : string =
-    Pos.show item.pos + " " + Set.show1 Term.show item.lookahead + " -> {" + item.mark + "}"
+    item.mark + ".\t" + NonTerm.show item.entity + " = " + Pos.show item.pos + " --- " + Set.show1 Term.show item.lookahead
   
+exception RuleIsNotDefined of NonTerm.t
+
+/// Parser state.
+///
+module rec State =
+
+  /// Contains all the positions the parser is in now.
+  ///
+  /// For instance, if parser is in
+  ///   F = [A] b --- d e
+  /// and
+  ///   A = c
+  /// then the state of the parser will be
+  ///   F = [A] b --- d e;
+  ///   A = [c]   --- b   
+  ///
+  /// That is because [A] is an entity of length > 1, we will go into on any FIRST(A).
+  ///
+  type t = Item.t Set
+
+  /// Type to collect all states into.
+  ///
+  type reg =
+    { indices : (State.t, int) Map
+      states  : (int, State.t) Map
+      counter : int
+    }
+
+  let emptyReg : reg =
+    { indices = Map.empty
+      states  = Map.empty
+      counter = 0
+    }
+
+  let add (state : State.t) (reg : State.reg) : int * State.reg * bool =
+    match reg.indices |> Map.tryFind state with
+    | Some existing -> existing, reg, true
+    | None ->
+      reg.counter,  
+        { reg with
+            indices = Map.add state reg.counter reg.indices
+            states  = Map.add reg.counter state reg.states
+            counter = reg.counter + 1
+        }, false
+
+  /// Group items from state that differ only in lookaheads,
+  /// and join the groups into single items with combined lookaheads.
+  ///
+  let normalize (state : State.t) : State.t =
+    state
+      |> Set.toList
+      |> List.groupBy (fun item -> item.pos)
+      |> List.map (fun (_, items) ->
+          { List.head items with
+              lookahead =
+                items
+                  |> List.map (fun item -> item.lookahead)
+                  |> Set.unionMany
+          }
+       )
+      |> Set.ofList
+
+  /// Calculate CLOSURE of an item with given lookahead, using grammar and FIRST.
+  ///
+  let closureOf
+    (grammar   : Grammar.t)
+    (first     : First.t)
+    (item      : Item.t)
+               : State.t
+    =
+      let loop (state : State.t) : State.t =
+        state
+          |> Set.bind (fun item ->
+            match item.pos.locus with
+            | None             -> Set.empty                            // Finished items produce nothing.
+            | Some (Point.T _) -> Set.empty                            // Same for token-next ones.
+            | Some (Point.NT nonTerm) ->                               // Non-terminals produce initial states
+              match Map.tryFind nonTerm grammar with                   //  of their rules.
+              | None -> raise <| RuleIsNotDefined nonTerm
+              | Some rules ->
+                let localahead =                                       // To find the new lookahead, 
+                  match Item.next item |> Option.bind Item.locus with  // we check next locus of item:
+                  | None                 -> item.lookahead             //  if it's empty, we use current item's lookahead;
+                  | Some (Point.T  term) -> Set.ofList [term]          //  if it's term, we use it;
+                  | Some (Point.NT nt)   -> Map.lookup first nt        //  if it's non-term, we use its FIRST set,
+                
+                rules
+                  |> List.map (Item.start localahead)
+                  |> Set.ofList
+          )
+
+      Set.ofList [item]
+        |> Set.fixpoint loop
+        |> normalize
+
+  let firstState (grammar : Grammar.t) (first : First.t) : State.t =
+    Map.find NonTerm.S grammar
+      |> List.map (Item.start <| Set.ofList [Term.Eof])
+      |> Set.ofList
+      |> Set.bind (closureOf grammar first)
+
+
+  let show (state : State.t) : string =
+    state
+      |> Set.map Item.show
+      |> Set.toList
+      |> String.concat "\n"
+
+module rec Goto =
+  type t = ((int * Point.t), int) Map
+
+  let from
+    (grammar : Grammar.t)
+    (first   : First.t)
+             : Goto.t * State.reg
+    =
+      let closure = State.closureOf grammar first
+      
+      // Loop, while keeping a pool of new discovered states until no new state is found.
+      //
+      let rec loop
+        ( goto   : Goto.t
+        , reg    : State.reg
+        , states : int Set
+        )        : Goto.t * State.reg
+        =
+        
+          // Registed all moves fron the state and find new reachable states.
+          //
+          let moves
+            ( goto  : Goto.t
+            , reg   : State.reg
+            , next  : int Set
+            )
+            ( state : int)
+                    : Goto.t * State.reg * int Set
+            =
+              let itemset = reg.states.Item state
+          
+              // Get all State, Point, NextState to put into GOTO table.
+              //
+              let endRules =
+                itemset
+                  |> Set.map  (fun item -> Item.locus item, Item.next item)
+                  |> Set.bind (fun (locus, next) ->
+                    match locus, next with
+                    | Some locus, Some next -> Set.ofList [state, locus, closure next]
+                    | _                     -> Set.empty
+                  )
+
+              // Registes NextState from (State, Point, NextState), add it to GOTO.
+              //
+              // If the state is new, add it to the `next` set.
+              //
+              let addState
+                ( goto   : Goto.t
+                , reg    : State.reg
+                , next   : int Set
+                )
+                ( source : int
+                , point  : Point.t
+                , dest   : State.t
+                )        : Goto.t * State.reg * int Set
+                =
+                let ix, reg, existed = State.add dest reg
+                let goto = Map.add (source, point) ix goto
+                let next = if existed then next else Set.add ix next
+                goto, reg, next
+
+              endRules
+                |> Set.fold addState (goto, reg, next)
+
+          let (goto, reg, next) =
+            states
+              |> Set.fold moves (goto, reg, Set.empty)
+
+          if Set.isEmpty next
+          then goto, reg
+          else loop (goto, reg, next)
+
+      let state0 = State.firstState grammar first
+      let ix, reg, _ = State.emptyReg |> State.add state0
+
+      loop (Map.empty, reg, Set.ofList [ix])
+
 let t = Point.T << Term.Term << Lexeme.Concrete
 let d = Point.T << Term.Term << Lexeme.Category
 let n = Point.NT << NonTerm.NonTerm
 let n1 = NonTerm.NonTerm
 let s = Point.NT NonTerm.S
-let r (m : string) (pt : Point.t list) : Rule.t =
-  { points = pt
-    mark   = m
-  }
+
 
 let map = Map.ofList
 let set = Set.ofList
 
 let arith
   : Grammar.t
-  = Map.ofList
-      [ NonTerm.S, [r "s" [n "E"]]
-        n1 "E",    [r "plus" [n "E"; t "+"; n "T"]; r "t" [n "T"]]
-        n1 "T",    [r "mult" [n "T"; t "*"; n "F"]; r "f" [n "F"]]
-        n1 "F",    [r "num" [d "num"]; r "group" [t "("; n "E"; t ")"]]
-      ]
+  = Map.empty
+    |> Grammar.addS     "s"    [n "E"]
 
+    |> Grammar.add  "E" "plus" [n "E"; t "+"; n "T"]
+    |> Grammar.add  "E" "t"    [n "T"]
+
+    |> Grammar.add  "T" "mult" [n "T"; t "*"; n "F"]
+    |> Grammar.add  "T" "f"    [n "F"]
+    
+    |> Grammar.add  "F" "num"  [d "num"]
+    |> Grammar.add  "F" "grp"  [t "("; n "E"; t ")"]
+
+arith
+  |> Grammar.show
+  |> printfn "GRAMMAR\n%s\n"
+  
 let first
   : First.t
   = arith
@@ -375,7 +614,7 @@ let first
 
 first
   |> First.show
-  |> printfn "%s"
+  |> printfn "FIRST\n%s\n"
 
 let follow
   : Follow.t
@@ -383,16 +622,19 @@ let follow
 
 follow
   |> Follow.show
-  |> printfn "%s"
+  |> printfn "FOLLOW\n%s\n"
 
-arith
-  |> Grammar.show
-  |> printfn "%s"
-  
 let rule = [n "E"; t "+"; n "T"]
 
-printfn "%s" (Pos.start rule |> Pos.show)
-printfn "%s" (Pos.start rule |> Pos.next |> Pos.showOption)
-printfn "%s" (Pos.start rule |> Pos.next |> Option.bind Pos.next |> Pos.showOption)
-printfn "%s" (Pos.start rule |> Pos.next |> Option.bind Pos.next |> Option.bind Pos.next |> Pos.showOption)
-printfn "%s" (Pos.start rule |> Pos.next |> Option.bind Pos.next |> Option.bind Pos.next |> Option.bind Pos.next |> Pos.showOption)
+let closure = State.closureOf arith first
+
+{ Rule.entity = NonTerm.NonTerm "F"
+  Rule.mark   = "grp"
+  Rule.points = [t "("; n "E"; t ")"]
+}
+  |> Item.start (Set.ofList [Term.Term <| Lexeme.Concrete "*"])
+  |> Item.next
+  |> Option.unsafeUnwrap
+  |> closure
+  |> State.show
+  |> printfn "%s" 
